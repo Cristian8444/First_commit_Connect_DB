fe47eb959ecd555cd36227fdd9561451ca696cd8
DESKTOP-ULPKO93\Cristian&Raluca <mojo_jojo844@yahoo.com>
2021-07-07 15:09:51 +0300

    This is for learning how to use connections to two different database.

--- src/main/java/siit/db/DbMain.java
+++ src/main/java/siit/db/DbMain.java
@@ -2,29 +2,37 @@ package siit.db;
 
 import org.h2.jdbcx.JdbcDataSource;
 
+import javax.sql.DataSource;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DbMain {
 
     public static void main(String[] args) {
 
+        try (
+            Connection connection = getPostGresSqlDataSourceConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from customers")
+        )
+        {
+            while(rs.next()){
+                System.out.println("customer { "+ rs.getInt("ID") + ", " + rs.getString("name") + " }");
+              }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static DataSource getDataSource(){
         JdbcDataSource dataSource = new JdbcDataSource();
         dataSource.setUrl("jdbc:h2:file:./db/store");
         dataSource.setUser("sa");
         dataSource.setPassword("");
 
-        try (
-            Connection connection = dataSource.getConnection();
-            Statement stmt = connection.createStatement()
-            ) {
-
-            //TODO interact with DB
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        return dataSource;
+    }
+    public static Connection getPostGresSqlDataSourceConnection()throws SQLException{
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","123456");
     }
 
 }
